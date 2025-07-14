4128fa013f8668efcfef1cd71c38633a54978097
Fabio de Almeida <fabio@adbits.com.br>
2025-07-14 17:58:18 -0300

    i3 theme / startup.sh

--- i3/.config/i3/startup.sh
+++ i3/.config/i3/startup.sh
@@ -14,3 +14,10 @@ xinput set-prop pointer:"Logitech G703 LS" "libinput Accel Profile Enabled" 0, 1
 # Opcional: zerar velocidade (neutro)
 xinput set-prop pointer:"Logitech G703 LS" "libinput Accel Speed" 0
 
+# Exec keyboard layout switcher us <-> us, us intl variant
+# setxkbmap -layout us,us -variant intl, -option grp:alt_shift_toggle
+
+# GNOME Keyring (corrigido)
+eval $(/usr/bin/gnome-keyring-daemon --start --components=pkcs11,secrets,ssh,gpg)
+export SSH_AUTH_SOCK
+
